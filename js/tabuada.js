22cd23fcbf03d92c6072151e577a328bdbc85edd
LuizCleone <luizcle_1@hotmail.com>
2025-08-29 20:56:48 -0300

    Tabuada

---
+++ js/tabuada.js
@@ -0,0 +1,47 @@
+// Seleção de Elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
+const multiplicationTable = document.querySelector("#multiplication-operations")
+// Funções 
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 0; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+
+        const template = `<div class="row">
+                <div class="operations">${number} x ${i} = </div>
+                <div class="result">${result}</div>
+            </div>`;
+
+        const parser = new DOMParser();
+
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
+
+    }
+
+    multiplicationTitle.innerText = number;
+};
+
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const multiplicatioNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if (!multiplicatioNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicatioNumber, multiplicatorNumber)
+
+})
